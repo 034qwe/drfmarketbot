b8b14daf919c3dde819ee7f67e2fb50cc87de865
034qwe <bnestercuk91@gmail.com>
2023-06-11 21:18:43 +0300

    reg first func

--- tgbot/bot.py
+++ tgbot/bot.py
@@ -1,13 +1,13 @@
 from aiogram.utils import executor
 from createbot import dp
-
+from handlers import myorder
 
 
 async def on_startup(_):
     print('bot start')
 
 
-
+myorder.register_handlers_myorder(dp)
 
     
 executor.start_polling(dp, skip_updates=True,on_startup=on_startup)
