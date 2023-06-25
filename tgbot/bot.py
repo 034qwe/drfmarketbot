ec8da1aa8f1eee3c3292023a2a0d95c2cdd7513c
034qwe <bnestercuk91@gmail.com>
2023-06-25 21:20:17 +0300

    dell trash

--- tgbot/bot.py
+++ tgbot/bot.py
@@ -27,22 +27,3 @@ if __name__ == "__main__":
 
 
 
-
-
-# from aiogram.utils import executor
-# from createbot import dp
-# from handlers import myorder
-# from aiogram.contrib.fsm_storage.memory import MemoryStorage
-
-
-# async def on_startup(_):
-#     print('bot start')
-
-
-
-
-
-# myorder.register_handlers_myorder(dp)
-
-    
-# executor.start_polling(dp, skip_updates=True,on_startup=on_startup)
